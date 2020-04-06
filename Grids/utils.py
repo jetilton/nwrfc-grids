556938f241d7c94bf7f394cdaed2459e20114566
jetilton <jfftilton@gmail.com>
2020-04-06 09:54:27 -0700

    changed directory structure

---
+++ Grids/utils.py
@@ -0,0 +1,17 @@
+import functools
+from functools import wraps
+
+
+def log_decorator(logger):
+    def real_decorator(function):
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            name = function.__name__
+            logger.info(f"Start {name}")
+            out = function(*args, **kwargs)
+            logger.info(f"End {name}")
+            return out
+
+        return wrapper
+
+    return real_decorator
